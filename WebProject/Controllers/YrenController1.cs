856919f43f1fcd44ccedbb776075d48c59d10e9e
Yaren Cerrah <yrn-61@hotmail.com.tr>
2024-12-09 13:17:29 +0300

    ikinci commit

---
+++ WebProject/Controllers/YrenController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebProject.Controllers
+{
+    public class YrenController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
